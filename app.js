a3d330e7e97c2f1b3fefe915e93e175690cf97d1
David Xie <david.scriptfan@gmail.com>
2016-05-22 18:34:34 +0800

    v1.0

---
+++ app.js
@@ -0,0 +1,17 @@
+function onClick(info, tab) {
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+  if (info.selectionText) {
+    var name = info.selectionText;
+    var url = info.pageUrl
+  } else {
+    var name = tab.title;
+    var url = tab.url;
+  }
+  var omnifocus_url = "omnifocus:///add?name=" + encodeURIComponent(name) + "&amp;note=" + encodeURIComponent(url);
+  document.body.insertAdjacentHTML("afterEnd", '<iframe src="' + omnifocus_url + '" style="display:none" />');
+}
+
+var button = chrome.contextMenus.create({"title": "Save to Omni Focus2",
+                     "contexts": ["page", "selection", "link"],
+                     "onclick": onClick});
